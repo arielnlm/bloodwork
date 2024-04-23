ceb9f43e07af02cc2c1a8ef0a9bb8051c8e60b7f
Lazar Dobrota <lazardobrota1323@gmail.com>
2024-04-23 19:11:42 +0200

    add chase speed and lost player metod for enemy

--- Assets/Enemy/Controls/EnemyMovement.cs
+++ Assets/Enemy/Controls/EnemyMovement.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -13,9 +14,9 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField] private Rigidbody2D _playerBody2D;
     [Range(0, 5)] [SerializeField] private float _idleTime = 1f;
     [Range(0, 5)] [SerializeField] private float _idleSearch = 1f;
-    
+
     [Header("Enemy Speed")]
-    [SerializeField] private float _speed;
+    [SerializeField] private float _searchSpeed;
     [SerializeField] private float chaseSpeed;
 
     [Header("Enemy Movement Range")]
@@ -26,13 +27,15 @@ public class EnemyMovement : MonoBehaviour
 
     private float _currentWaitTime = -1f;
     private float _currentSearchWaitTime = 0;
-    private bool _isChasing = false;
-    
+    private EnemyMode _enemyMode = EnemyMode.RegularSearch;
+    private float _speed;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
+        _speed = _searchSpeed;
         _currentWaitTime = _idleTime;
         _enemyBody2D.velocity = new Vector2(fieldOfView.directionRight ? _speed : -_speed, _enemyBody2D.velocity.y);
     }
@@ -41,15 +44,14 @@ public class EnemyMovement : MonoBehaviour
     void Update()
     {
 
-        if (_isChasing)
+        switch(_enemyMode)
         {
-            Chase();
-        }
-        else
-        {
-            Search();
+            case EnemyMode.Chase: Chase(); break;
+            case EnemyMode.LostPlayer: LostPlayer(); break;
+            case EnemyMode.RegularSearch: RegularSearch(); break;
         }
     }
+
 
     private void Chase()
     {
@@ -75,15 +77,32 @@ public class EnemyMovement : MonoBehaviour
         }
     }
 
-    private void Search()
+    private void LostPlayer()
     {
         if (fieldOfView.canSeePLayer)
         {
             startChaseMovement();
-            fieldOfView.ChaseMode();
-            _isChasing = true;
             return;
         }
+
+        _enemyBody2D.velocity = Vector2.zero;
+        _currentSearchWaitTime -= Time.deltaTime;
+        if (_currentSearchWaitTime < 0)
+        {
+            _enemyMode = EnemyMode.RegularSearch;
+            UpdateSpeed(_speed);
+        }
+    }
+
+    private void RegularSearch()
+    {
+        if (fieldOfView.canSeePLayer)
+        {
+            startChaseMovement();
+            return;
+        }
+
+
 
         if (_enemyBody2D.position.x >= _maxRight.position.x && fieldOfView.directionRight ||
             _enemyBody2D.position.x <= _maxLeft.position.x  && !fieldOfView.directionRight)
@@ -102,21 +121,38 @@ public class EnemyMovement : MonoBehaviour
     {
         fieldOfView.directionRight = !fieldOfView.directionRight;
 
-        _enemyBody2D.velocity = new Vector2(fieldOfView.directionRight ? _speed : -_speed, _enemyBody2D.velocity.y);
+        UpdateSpeed(_speed);
         _currentWaitTime = _idleTime;
     }
 
     private void startChaseMovement()
     {
-        _enemyBody2D.velocity = new Vector2(fieldOfView.directionRight ? _speed : -_speed, _enemyBody2D.velocity.y);
+        _speed = chaseSpeed;
+        UpdateSpeed(_speed);
+        fieldOfView.ChaseMode();
+        _enemyMode = EnemyMode.Chase;
     }
     private void stopChaseMovement()
     {
+        _speed = _searchSpeed;
+        UpdateSpeed(_speed);
         fieldOfView.SearchMode();
-        _isChasing = false;
+        _enemyMode = EnemyMode.LostPlayer;
 
        // _enemyBody2D.velocity = Vector2.zero;
         _currentSearchWaitTime = _idleSearch;
     }
 
+    private void UpdateSpeed(float speed)
+    {
+        _enemyBody2D.velocity = new Vector2(fieldOfView.directionRight ? speed : -speed, _enemyBody2D.velocity.y);
+    }
+
+    private enum EnemyMode
+    {
+        Chase,
+        LostPlayer,
+        RegularSearch
+
+    }
 }
