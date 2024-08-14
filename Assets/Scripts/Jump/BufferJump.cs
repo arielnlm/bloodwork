a15abfd50c072b247bf826a39f9920c0f97eeb64
Nemanja Radovanovic <nradovanovic12022rn@raf.rs>
2024-08-14 03:57:56 +0200

    implement Buffer Jump

--- Assets/Scripts/Jump/BufferJump.cs
+++ Assets/Scripts/Jump/BufferJump.cs
@@ -1,10 +1,47 @@
-﻿namespace BloodWork.Jump
+﻿using BloodWork.Commons;
+using UnityEngine;
+
+namespace BloodWork.Jump
 {
     public class BufferJump : AbstractJump
     {
+        [SerializeField] private float m_BufferTimeLimit = 0.12f;
+
+        private float m_BufferTime;
+
         private void Update()
         {
-            //TODO: Implementation
+            m_BufferTime = TriggerState == TriggerState.Start ? 0 : m_BufferTime + Time.deltaTime;
+
+            if (!IsJumpHolder && JumpState != JumpState.Default)
+                return;
+
+            if (Utils.IsChanged(ref ApplyJumpForce, ShouldApplyJumpForce()))
+                JumpTime = 0;
+        }
+
+        private bool ShouldApplyJumpForce()
+        {
+            return ApplyJumpForce && !IsCeilingHit() && (!IsMinimumJumpTimePassed() || (TriggerState == TriggerState.Continue && !IsExtendedTimePassed())) ||
+                   !ApplyJumpForce && IsOnGround() && !IsBufferTimePassed();
+        }
+
+        private void FixedUpdate()
+        {
+            if (JumpState == JumpState.Default && !ApplyJumpForce)
+                return;
+
+            if (ApplyJumpForce)
+                Entity.Rigidbody.velocity = new Vector2(Entity.Rigidbody.velocity.x, JumpForce);
+
+            JumpTime += Time.fixedDeltaTime;
+
+            NotifyCurrentState();
+        }
+
+        private bool IsBufferTimePassed()
+        {
+            return m_BufferTime > m_BufferTimeLimit;
         }
     }
 }
