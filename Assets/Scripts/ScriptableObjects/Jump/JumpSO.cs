14fa9c15366e1cde290b7774b65db0882cd9e35e
Nemanja Radovanovic <nradovanovic12022rn@raf.rs>
2024-09-04 01:51:24 +0200

    implement SharedSO

--- Assets/Scripts/ScriptableObjects/Jump/JumpSO.cs
+++ Assets/Scripts/ScriptableObjects/Jump/JumpSO.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using UnityEditor;
 using UnityEngine;
 
 namespace BloodWork.ScriptableObjects.Jump
 {
-    public abstract class JumpSO : ScriptableObject
+    public abstract class JumpSO : SharedSO
     {
         private static float s_JumpForce;
         private static float s_ExtendJumpTimeLimit;
@@ -14,51 +13,33 @@ namespace BloodWork.ScriptableObjects.Jump
         [field: Header("Shared Properties")]
         [field: SerializeField] public float JumpForce           { get; private set; }
         [field: SerializeField] public float ExtendJumpTimeLimit { get; private set; }
-        
-        private void OnEnable()
-        {
-            SyncAndSave();
 
-            OnJumpValuesChange += SyncAndSave;
+
+        protected override void Subscribe()
+        {
+            OnJumpValuesChange += SyncLocalValuesAndSave;
         }
 
-        private void OnDisable()
+        protected override void Unsubscribe()
         {
-            OnJumpValuesChange -= SyncAndSave;
+            OnJumpValuesChange -= SyncLocalValuesAndSave;
         }
 
-        private void OnValidate()
+        protected override void Dispatch()
         {
-            UpdateSharedValues();
-    
             OnJumpValuesChange?.Invoke();
         }
-        
-        protected virtual void UpdateSharedValues()
+
+        protected override void SyncSharedValues()
         {
             s_JumpForce           = JumpForce;
             s_ExtendJumpTimeLimit = ExtendJumpTimeLimit;
         }
-        
-        protected virtual void SyncAndSave()
+
+        protected override void SyncLocalValues()
         {
-            if (!IsValid())
-                return;
-            
             JumpForce           = s_JumpForce;
             ExtendJumpTimeLimit = s_ExtendJumpTimeLimit;
-            
-            EditorUtility.SetDirty(this);
-        }
-        
-        private bool IsValid()
-        {
-            if (this)
-                return true;
-            
-            OnJumpValuesChange -= SyncAndSave;
-            
-            return false;
         }
     }
 }
---
+++ Assets/Scripts/ScriptableObjects/SharedSO.cs
@@ -0,0 +1,79 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace BloodWork.ScriptableObjects
+{
+    public abstract class SharedSO : ScriptableObject
+    {
+        protected virtual void OnEnable()
+        {
+            SyncLocalValuesAndSave();
+            Subscribe();
+        }
+
+        protected virtual void OnDisable()
+        {
+            Unsubscribe();
+        }
+
+        protected virtual void OnValidate()
+        {
+            SyncSharedValues();
+            Dispatch();
+        }
+
+        /// <summary>
+        /// Synchronises local with shared variables and mark the asset as ready to write on disk.
+        /// </summary>
+        protected virtual void SyncLocalValuesAndSave()
+        {
+            if (!IsValid())
+                return;
+
+            SyncLocalValues();
+            
+            EditorUtility.SetDirty(this);
+        }
+        
+        /// <summary>
+        /// Checks the object's validity state, and unsubscribes invalid object if autoUnsubscribe is true.
+        /// </summary>
+        /// <param name="autoUnsubscribe">whether null instance should auto unsubscribe</param>
+        /// <returns>Returns true if object is not null, false otherwise</returns>
+        protected bool IsValid(bool autoUnsubscribe = true)
+        {
+            if (this)
+                return true;
+            
+            if (autoUnsubscribe) 
+                Unsubscribe();
+            
+            return false;
+        }
+        
+        /// <summary>
+        /// Its called whenever an object needs to subscribe to an event.
+        /// </summary>
+        protected abstract void Subscribe();
+        
+        /// <summary>
+        /// Its called whenever an object needs to unsubscribe from an event.
+        /// </summary>
+        protected abstract void Unsubscribe();
+        
+        /// <summary>
+        /// Its called whenever the object needs to notify subscribers of an event.
+        /// </summary>
+        protected abstract void Dispatch();
+        
+        /// <summary>
+        /// Its called whenever local variables change and shared variables need to be synchronized.
+        /// </summary>
+        protected abstract void SyncSharedValues();
+        
+        /// <summary>
+        /// Its called whenever shared variables change and local variables need to be synchronized.
+        /// </summary>
+        protected abstract void SyncLocalValues();
+    }
+}
